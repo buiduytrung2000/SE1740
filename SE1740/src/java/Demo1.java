5cca0b9cea364ce5efbe89a5ae2287d9888c2b65
buiduytrung2000 <77658469+buiduytrung2000@users.noreply.github.com>
2023-05-31 07:58:18 +0700

    upload docs

---
+++ SE1740/src/java/Demo1.java
@@ -0,0 +1,35 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author buidu
+ */
+public class Demo1 extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Demo</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Demo at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
